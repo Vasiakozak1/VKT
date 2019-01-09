7dc5e662d913a386e950a69bb78cf68b76a599a4
vasiakozak1 <junglehunter2707@gmail.com>
2019-01-09 18:19:23 +0200

    Implemented application menu

--- app/src/main/java/com/example/admin/vktargetapp/LoginFragment.java
+++ app/src/main/java/com/example/admin/vktargetapp/LoginFragment.java
@@ -3,16 +3,38 @@ package com.example.admin.vktargetapp;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginFragment extends Fragment {
+
+    TextInputEditText emailInput;
+    TextInputEditText passwordInput;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.login_fragment, container, false);
+
+
+
+        final View view = inflater.inflate(R.layout.login_fragment, container, false);
+        emailInput = view.findViewById(R.id.emailInput);
+        passwordInput = view.findViewById(R.id.passwordInput);
+
+        final Button loginButton = view.findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = emailInput.getText().toString();
+                final String password = passwordInput.getText().toString();
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/example/admin/vktargetapp/MainActivity.java
+++ app/src/main/java/com/example/admin/vktargetapp/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.admin.vktargetapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,11 +22,40 @@ import org.jsoup.nodes.Document;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements NavigationHost {
+    private DrawerLayout appNavigationDrawer;
+    private NavigationView appNavigationView;
+    private Toolbar appToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        appNavigationDrawer = findViewById(R.id.drawer_layout);
+        appNavigationView = findViewById(R.id.nav_view);
+        appToolbar = findViewById(R.id.main_toolbar);
+
+        setSupportActionBar(appToolbar);
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        appNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                // set item as selected to persist highlight
+                menuItem.setChecked(true);
+                // close drawer when item is tapped
+                appNavigationDrawer.closeDrawers();
+
+                // Add code here to update the UI based on the item selected
+                // For example, swap UI fragments here
+
+                return true;
+            }
+        });
+
+
+
         try{
             if(UserData.APIKey == null) {
                 Login();
@@ -50,5 +86,15 @@ public class MainActivity extends AppCompatActivity implements NavigationHost {
             transaction.addToBackStack(null);
         }
         transaction.commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                appNavigationDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
